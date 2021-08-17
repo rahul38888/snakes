263896806da62070ac92ee087cfb9f109af5e90d
rahul.s <rahul.s@timesinternet.in>
2021-08-18 01:26:26 +0530

    cell, linked list, snake and board ready

--- src/objects/board.py
+++ src/objects/board.py
@@ -4,4 +4,7 @@ from cell_state import CellState
 
 class Board:
     def __init__(self, size):
-        self.grip = [[Cell(i, j, CellState.EMPTY) for j in range(size)] for i in range(size)]
+        self.grid = [[Cell(i, j, CellState.EMPTY) for j in range(size)] for i in range(size)]
+
+    def get_cell(self, x, y):
+        return self.grid[x][y]
